fb5946a736d18c0fdc09dddbfb312652e30d942f
yurii <benisones@gmail.com>
2023-07-11 13:15:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
-from app.services.bla import (
-    bla1,
-    bla2,
-)
-from app.services.check_file_exists import check_file_exists
-from app.services.show_greeting import show_greeting
+import os
+from faker import Faker
+
+def show_greeting() -> [str]:
+
+    fake = Faker()
+    name = fake.name()
+    address = fake.address()
+
+    print(f'Hello, my name is {name}!\nI live in {address}\n')
+
+
+def show_current_directory() -> [str]:
+    cur_directory = os.getcwd()
+    print(f'We work in {cur_directory}')
 
 
 def main():
     show_greeting()
-    check_file_exists()
-
-    bla1()
-    bla2(
-        foo_1=1,
-        #
-        foo_3=3,
-    )
+    show_current_directory()
